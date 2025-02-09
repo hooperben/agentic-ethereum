fb8d3cb3060c622f3c0c03d41386eb98bed8feea
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-02-09 14:34:31 +1000

    feat: working through contracts

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -3,24 +3,87 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 
-use alloy_primitives::{Address, U256};
-use openzeppelin_stylus::token::erc20::{extensions::Erc20Metadata, Erc20};
-use stylus_sdk::prelude::{entrypoint, public, storage};
+use alloy_primitives::{Address, U160, U256};
+use openzeppelin_stylus::{
+    access::ownable,
+    token::erc20::{extensions::Erc20Metadata, Erc20, IErc20},
+};
+use stylus_sdk::{
+    call, contract, msg,
+    prelude::{entrypoint, public, sol_interface, storage, SolidityError},
+    storage::{StorageMap, StorageU256},
+};
 
 #[entrypoint]
 #[storage]
-struct Erc20Example {
+struct SmartVault {
     #[borrow]
-    pub erc20: Erc20,
-    #[borrow]
-    pub metadata: Erc20Metadata,
+    pub ownable: ownable::Ownable,
+
+    // Token -> Owner -> Amount
+    pub balances: StorageMap<Address, StorageMap<Address, StorageU256>>,
+}
+
+sol_interface! {
+    interface IERC20 {
+        function total_supply() external returns (uint256);
+        function transfer(address to, uint256 value) external returns (bool);
+        function transfer_from(address from, address to, uint256 value) external returns (bool);
+    }
+}
+
+/// An error that occurred in the [`VestingWallet`] contract.
+#[derive(SolidityError, Debug)]
+pub enum Error {
+    TransferFailed(call::Error),
+    // /// Error type from [`SafeErc20`] contract [`safe_erc20::Error`].
+    // SafeErc20(safe_erc20::Error),
+    // /// The token address is not valid. (eg. `Address::ZERO`).
+    // InvalidToken(InvalidToken),
+}
+
+impl SmartVault {
+    pub fn zero_address(&self) -> Address {
+        Address::from(U160::from(0x0))
+    }
 }
 
 #[public]
-#[inherit(Erc20, Erc20Metadata)]
-impl Erc20Example {
-    pub fn mint(&mut self, account: Address, value: U256) -> Result<(), Vec<u8>> {
-        self.erc20._mint(account, value)?;
+impl SmartVault {
+    #[payable]
+    pub fn deposit(&mut self, erc20: IERC20, mut amount: U256) -> Result<(), Error> {
+        if erc20.address == self.zero_address() {
+            if msg::value() != amount {
+                amount = msg::value();
+            }
+        } else {
+            erc20
+                .transfer_from(&mut *self, msg::sender(), contract::address(), amount)
+                .map_err(|e| Error::TransferFailed(call::Error::from(e)))?;
+        }
+
+        // Increase the balance of the sender
+        self.balances
+            .setter(erc20.address)
+            .setter(msg::sender())
+            .set(amount);
+
         Ok(())
+    }
+
+    pub fn withdraw(&mut self, erc20: IERC20, amount: U256) -> Result<(), Error> {
+        if erc20.address == self.zero_address() {
+            // self.withdraw_native(amount);
+        } else {
+            erc20
+                .transfer(self, msg::sender(), amount)
+                .map_err(|e| Error::TransferFailed(call::Error::from(e)))?;
+        }
+
+        Ok(())
+    }
+
+    fn owner(&self) -> Address {
+        self.ownable.owner()
     }
 }
